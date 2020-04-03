be7813c9c27358605a6147ba17b4ec5f085a5087
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-04-03 09:26:47 +0200

    [0.12] fix creator

---
+++ src/js/components/Post/youtube.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { imagePostUrl, domainUrl } from "./../../constants/types";
+
+const ContentYoutube = props => {
+  const post = props.post;
+console.log('post',post);
+  return (
+    <div className="post-content__youtube">
+      <iframe
+        width="100%"
+        height="215"
+        src={"https://www.youtube.com/embed/" + post.youtube}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default ContentYoutube;
+
+// background: url(images/bg.jpg) no-repeat center center fixed;
