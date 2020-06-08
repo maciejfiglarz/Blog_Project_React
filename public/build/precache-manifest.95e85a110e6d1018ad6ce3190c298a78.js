61aea996d39b55a268d471bd9fb79b6d06bfdfee
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-06-08 20:15:55 +0200

    to build

---
+++ public/build/precache-manifest.95e85a110e6d1018ad6ce3190c298a78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9eacc7241ab6f08445487c443f347b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "160e8b0847424fbf4849",
+    "url": "./static/css/main.8813e59d.chunk.css"
+  },
+  {
+    "revision": "98db6e7e568044520583",
+    "url": "./static/js/2.40bd61da.chunk.js"
+  },
+  {
+    "revision": "3efc19061e29835a4cd33c09d37cdc0b",
+    "url": "./static/js/2.40bd61da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160e8b0847424fbf4849",
+    "url": "./static/js/main.655ee7de.chunk.js"
+  },
+  {
+    "revision": "77fe357d52a63d5f32f7",
+    "url": "./static/js/runtime-main.83fb1e46.js"
+  },
+  {
+    "revision": "32a1510a5c9b1d0452b80aa80428ae4b",
+    "url": "./static/media/background.32a1510a.png"
+  },
+  {
+    "revision": "3db1bd59a5d391dec1a363b81bb11ea1",
+    "url": "./static/media/create_action.3db1bd59.png"
+  },
+  {
+    "revision": "2dd0144c8eae5904ec54047c9544e5bd",
+    "url": "./static/media/creator_logo.2dd0144c.png"
+  },
+  {
+    "revision": "bc4cd4ca969b57050902471b059f087a",
+    "url": "./static/media/footer_logo.bc4cd4ca.png"
+  },
+  {
+    "revision": "e7b745b026a478c6f2e583fc9e19a0b5",
+    "url": "./static/media/logo_desktop.e7b745b0.png"
+  },
+  {
+    "revision": "4bd986b7de281f9dda2e2bbc9205a75e",
+    "url": "./static/media/logo_mobile.4bd986b7.png"
+  }
+]);
