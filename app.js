7785b1fb743db62ef9458c2aadffe2c9198b80cf
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-06-08 22:20:30 +0200

    m

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
   
   mongoose.connect("mongodb://localhost:27017", {
