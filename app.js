9e7ec219e50999e9a8832090b16e48f29933495f
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-06-08 20:18:08 +0200

    public

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ console.log('process.env.NODE_ENV',process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
   });
 }
 
