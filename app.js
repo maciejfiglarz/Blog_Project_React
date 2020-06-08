23df6960c71f10445b3b280ae4de9a6c7e2fabf2
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-06-08 20:28:49 +0200

    mongoose

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const config = require('config');
+const config = require("config");
 const bodyParser = require("body-parser");
-const multer = require('multer')
+const multer = require("multer");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 const postRouter = require("./api/routes/post");
 const fileRouter = require("./api/routes/file");
@@ -20,7 +16,6 @@ const commentRouter = require("./api/routes/comment");
 const userRouter = require("./api/routes/user");
 const voteRouter = require("./api/routes/vote");
 const postMenagerRouter = require("./api/routes/post-menager");
-
 
 const adminPostRouter = require("./admin/routes/post");
 const adminIndexRouter = require("./admin/routes/index");
@@ -31,7 +26,7 @@ const morgan = require("morgan");
 var app = express();
 
 var Twig = require("twig");
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = "production";
 
 global.baseUrl = "http://localhost:5000";
 
@@ -53,17 +48,16 @@ app.use(
 );
 app.use(bodyParser.json());
 
+var gulp = require("gulp");
+var bs = require("browser-sync").create(); // create a browser sync instance.
 
-var gulp = require('gulp');
-var bs = require('browser-sync').create(); // create a browser sync instance.
-
-gulp.task('browser-sync', function() {
-    bs.init({
-        server: {
-            baseDir: "./"
-        },
-        proxy: "localhost:5000" 
-    });
+gulp.task("browser-sync", function () {
+  bs.init({
+    server: {
+      baseDir: "./",
+    },
+    proxy: "localhost:5000",
+  });
 });
 
 // view engine setup
@@ -72,7 +66,7 @@ app.set("view engine", "twig");
 
 app.set("twig options", {
   allow_async: false, // Allow asynchronous compiling
-  strict_variables: false
+  strict_variables: false,
 });
 
 app.use(logger("dev"));
@@ -93,11 +87,21 @@ app.use("/post-menager", postMenagerRouter);
 app.use("/admin/post", adminPostRouter);
 app.use("/admin/user", adminUserRouter);
 app.use("/admin", adminIndexRouter);
-console.log('process.env.NODE_ENV',process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "build", "index.html"));
+  });
+  
+  mongoose.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  mongoose.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 }
 
@@ -106,15 +110,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-
-
-
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, console.log(`Listening on port ${PORT}...`));
 
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
