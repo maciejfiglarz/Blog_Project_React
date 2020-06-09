b4baee74ec983f6e3ca4916030787b5d553a40e7
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-06-09 08:32:45 +0200

    push

--- app.js
+++ app.js
@@ -105,9 +105,9 @@ app.use("/admin/user", adminUserRouter);
 app.use("/admin", adminIndexRouter);
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
 
